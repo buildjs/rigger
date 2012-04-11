99d4b38dc96a4da1efaaaf1defa05d39e5aba66c
Damon Oehlman <damon.oehlman@sidelab.com>
2012-04-11 10:31:56 +1000

    Added some dynamic tests

--- test/local.js
+++ test/local.js
@@ -1,12 +1,26 @@
-var rigger = require('../lib/rigger'),
+var expect = require('expect.js'),
+    rigger = require('..'),
     fs = require('fs'),
-    path = require('path');
+    path = require('path'),
+    inputPath = path.resolve(__dirname, 'input'),
+    outputPath = path.resolve(__dirname, 'output');
 
-describe('local rigging tests', function() {
-    it('should be able to parse a simple file', function(done) {
-        rigger.readFile(path.resolve(__dirname, 'samples/local.js'), 'utf8', function(err, data) {
-            console.log(data);
-            done(err);
+// run tests for each of the input files
+fs.readdir(inputPath, function(err, files) {
+    describe('local rigging tests', function() {
+        (files || []).forEach(function(file) {
+            it('should be able to parse: ' + file, function(done) {
+                // read the output file
+                fs.readFile(path.join(outputPath, file), 'utf8', function(refErr, reference) {
+                    expect(err).to.not.be.ok();
+
+                    rigger.readFile(path.join(inputPath, file), 'utf8', function(parseErr, parsed) {
+                        expect(parsed).to.equal(reference);
+                    
+                        done(parseErr);
+                    });
+                });
+            });
         });
     });
 });
