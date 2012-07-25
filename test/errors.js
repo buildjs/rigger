a011b367ca6a3cc559200131a2092b9c4e0fd168
Damon Oehlman <damon.oehlman@sidelab.com>
2012-07-26 09:59:01 +1000

    Validated that conversion errors produce an error

--- test/errors.js
+++ test/errors.js
@@ -2,7 +2,11 @@ var expect = require('expect.js'),
     rigger = require('..'),
     fs = require('fs'),
     path = require('path'),
-    inputPath = path.resolve(__dirname, 'input-errors');
+    inputPath = path.resolve(__dirname, 'input-errors'),
+    targetContext = {
+        coffee: '.js',
+        styl: '.css'
+    };
 
 // run tests for each of the input files
 fs.readdir(inputPath, function(err, files) {
@@ -11,11 +15,12 @@ fs.readdir(inputPath, function(err, files) {
         // create a test for each of the input files
         (files || []).forEach(function(file) {
             it('should produce an error when rigging: ' + file, function(done) {
-                var targetPath = path.join(inputPath, file);
+                var targetExt = targetContext[path.extname(file).slice(1)] || path.extname(file),
+                    targetPath = path.join(inputPath, file);
                 
                 fs.stat(targetPath, function(err, stats) {
                     if ((! err) && stats.isFile()) {
-                        rigger(path.join(inputPath, file), function(parseErr, parsed) {
+                        rigger(targetPath, { targetType: targetExt }, function(parseErr, parsed) {
                             expect(parseErr).to.be.ok();
                             done();
                         });
