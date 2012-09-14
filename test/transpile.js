1faebb2ec48530de71546078fdf29b01cd536370
Damon Oehlman <damon.oehlman@sidelab.com>
2012-09-14 13:16:33 +1000

    Added support for converting PEG.js grammars => js files

--- test/transpile.js
+++ test/transpile.js
@@ -16,9 +16,18 @@ var assert = require('assert'),
     },
     targetContext = {
         coffee: '.js',
-        styl: '.css'
+        roy: '.js',
+        styl: '.css',
+        pegjs: '.js'
     },
-    files = fs.readdirSync(inputPath);
+    
+    // find the files to read
+    files = fs.readdirSync(inputPath),
+    
+    reIgnoreFiles = /^.DS_Store/i,
+    isIncluded = function(file) {
+        return !reIgnoreFiles.test(file);
+    };
     
 // override squirrel default functional allowing installation
 squirrel.defaults.allowInstall = true;
@@ -60,11 +69,11 @@ function rigAndCompare(file, done) {
 // run tests for each of the input files
 describe('transpiling tests', function() {
     // create a test for each of the input files
-    (files || []).forEach(function(file) {
+    (files || []).filter(isIncluded).forEach(function(file) {
         it('should be able to rig: ' + file, rigAndCompare.bind(null, file));
     });
 
     it('should be able to rig all local files in parallel', function(done) {
-        async.forEach(files, rigAndCompare, done);
+        async.forEach(files.filter(isIncluded), rigAndCompare, done);
     });
 });
