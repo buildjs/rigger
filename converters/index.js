4ce4477527ea67ca56332a592929fd9b7518b2ad
Damon Oehlman <damon.oehlman@sidelab.com>
2012-10-08 13:30:57 +1000

    Updated converter require logic to search cwd (and above) for required modules

--- converters/index.js
+++ converters/index.js
@@ -1,4 +1,22 @@
-var events = require('events');
+var events = require('events'),
+    path = require('path'),
+    _ = require('underscore');
+
+function requireLocal(target) {
+    var searchModules = require('module')._nodeModulePaths('.').map(function(modPath) {
+            return path.join(modPath, target);
+        });
+    
+    return [target].concat(searchModules).reduce(function(memo, targetPath) {
+        // attempt to include the module
+        try {
+            return memo || require(targetPath);
+        }
+        catch (e) {
+            return null;
+        }
+    }, null);
+}
 
 exports.require = function() {
     var emitter = new events.EventEmitter(),
@@ -6,11 +24,14 @@ exports.require = function() {
         modules;
       
     process.nextTick(function() {
-        try {
-            emitter.emit.apply(emitter, ['ok'].concat(args.map(require)));
+        var mods = args.map(requireLocal),
+            missingMods = _.reject(mods, _.identity);
+            
+        if (missingMods.length > 0) {
+            emitter.emit('error', new Error('Unable to load modules: ' + missingMods.toString()));
         }
-        catch (e) {
-            emitter.emit('error', e);
+        else {
+            emitter.emit.apply(emitter, ['ok'].concat(mods));
         }
     });
     
